0e79bd27563dfe9eced830b364a48338c9e794f8
Kevin-igraneza <207916721+Kevin-igraneza@users.noreply.github.com>
2026-04-09 11:11:14 +0200

    Initial Spring Boot and Thymeleaf setup

---
+++ src/main/java/com/carfix/carfixrwanda/CarfixrwandaApplication.java
@@ -0,0 +1,13 @@
+package com.carfix.carfixrwanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfixrwandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfixrwandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carfix/carfixrwanda/CarfixrwandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carfix.carfixrwanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfixrwandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

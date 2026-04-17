8fd0281ba5995b9fbf7c55fc5df2c25381ac0a3c
Kevin-igraneza <207916721+Kevin-igraneza@users.noreply.github.com>
2026-04-17 16:09:47 +0200

    Add some functinalities

--- README.md
+++
@@ -1,2 +0,0 @@
-# CarFix-Rwanda
-CarFix Rwanda is a Spring Boot web application that helps vehicle owners find the right mechanics based on specialization, supported car models, and repair needs
--- src/main/java/com/carfix/carfixrwanda/repository/MechanicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.carfix.carfixrwanda.repository;
-
-import com.carfix.carfixrwanda.model.Mechanic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MechanicRepository extends JpaRepository<Mechanic, Long> {
-}
